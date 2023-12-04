270b97d23f07270c5e4b73a0897e5d59eceac20b
DenisP666 <gamelomasterrr@icloud.com>
2023-12-04 21:52:01 +0300

    Add project files.

---
+++ Task5_1/Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+namespace Task5_1
+{
+    public class Program
+    {
+        static int Multiplier(int a, int b)
+
+        {
+            int c = b;
+            int Result = 0;
+            for (b = 1; b <= c; b++)
+            {
+                Result = a * b;
+                Console.WriteLine($"{a} * {b} = {Result}");
+            }
+            return Result;
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            int a, b;
+            Console.WriteLine("Enter first number");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number");
+            b = int.Parse(Console.ReadLine());
+            int Result = Multiplier(a, b);
+        }
+
+
+    }
+}
---
+++ Task5_3/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Task5_3
+{
+    internal class Program
+    {
+        static long Fib(int n)
+        {
+            
+            
+                if ((n == 1) || (n == 2))
+                    return 1;
+                return Fib(n - 1) + Fib(n - 2);
+            
+        }
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("Enter count of numbers");
+            int n = int.Parse(Console.ReadLine());
+            
+                       
+            for (int i = 1;i <= n; i++)
+            {
+                Console.WriteLine(Fib(i));
+                
+            }
+            
+        }
+    }
+}
