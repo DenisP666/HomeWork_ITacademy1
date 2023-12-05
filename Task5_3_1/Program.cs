6fc974e6f8b65cf535be8095b494be0159ec67f0
DenisP666 <gamelomasterrr@icloud.com>
2023-12-05 17:25:19 +0300

    FactorialUpdate

---
+++ Task5_3_1/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Task5_3_1
+{
+    internal class Program
+    {
+
+        static int factorial(int i)
+        {
+            int result;
+
+            if (i == 1)
+                return 1;
+            result = factorial(i - 1) * i;
+            return result;
+        }
+
+
+        static void Main(string[] args)
+        {
+            int i = int.Parse(Console.ReadLine());
+            Console.WriteLine(factorial(i));
+        }
+    }
+}
